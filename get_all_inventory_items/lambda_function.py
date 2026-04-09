63ec6a16fe51adcdf4107c492ae3e9e1a5d6bd91
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-04-09 16:59:42 -0500

    feat: add get_all_inventory_items lambda function

---
+++ get_all_inventory_items/lambda_function.py
@@ -0,0 +1,37 @@
+import json
+ 
+import boto3
+ 
+ 
+def lambda_handler(event, context):
+
+    dynamo_client = boto3.client("dynamodb")
+
+    table_name = "Inventory"
+ 
+    try:
+
+        response = dynamo_client.scan(TableName=table_name)
+
+        items = response["Items"]
+ 
+        return {
+
+            "statusCode": 200,
+
+            "body": json.dumps(items),
+
+        }
+
+    except Exception as e:
+
+        print(e)
+
+        return {
+
+            "statusCode": 500,
+
+            "body": json.dumps(str(e)),
+
+        }
+ 
